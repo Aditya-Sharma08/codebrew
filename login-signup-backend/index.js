3370172cf6b59c499fed2aa72349e75793cf81ee
Adi <adityasharma08official@gmail.com>
2023-09-25 03:35:43 +0530

    Updated Backend/index.js

--- login-signup-backend/index.js
+++ login-signup-backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://code-brew.netlify.app/"],
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
