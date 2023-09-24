80280697b1757780531533380a648bb5e82fb28d
Adi <adityasharma08official@gmail.com>
2023-09-25 02:58:01 +0530

    Updated Backend/index.js

--- login-signup-backend/index.js
+++ login-signup-backend/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json());
 express.urlencoded({ extended: true });
-app.use(cors());
 
 mongoose
   .connect(
